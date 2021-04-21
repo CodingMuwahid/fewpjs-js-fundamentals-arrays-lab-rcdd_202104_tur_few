3eb03638ea6bd27c3bfcebf9255ec198d63e6636
Isa Tekinkaya <isatekinkaya@yahoo.de>
2021-04-21 13:21:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const removeLast = ["Milo", "Otis", "Garfield"];
 const removeFirst = ["Milo", "Otis", "Garfield"];
 
 append.push("Odie");
-prepend.unshift("prepends");
+prepend.unshift("monkey");
 removeLast.pop();
 removeFirst.shift();
