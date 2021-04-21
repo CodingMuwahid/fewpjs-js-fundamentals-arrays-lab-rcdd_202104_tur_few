baafb2d09410445247aba5e547fc58801224d881
Isa Tekinkaya <isatekinkaya@yahoo.de>
2021-04-21 13:15:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // Write your solution here!
 const append = ["Milo", "Otis", "Garfield"];
 const prepend = ["Milo", "Otis", "Garfield"];
-const revoeLast = ["Milo", "Otis", "Garfield"];
+const removeLast = ["Milo", "Otis", "Garfield"];
 
 
 append.push("Odie");
+prepend.unshift("hiho");
